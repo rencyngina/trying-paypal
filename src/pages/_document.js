cbcf9c56018ce703c456ff3ef149a820834a0153
rencyngina <rencyngina@gmail.com>
2024-07-30 23:18:06 +0300

    document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Add any custom head tags here */}
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://example.com/styles.css" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            src="https://code.jivosite.com/widget/7K3PfvgiD7"
+            async
+          ></script>
+          <link rel="logo" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
