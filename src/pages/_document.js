6b08711a23aaa90c0b5593e5d61b7888946e8798
rencyngina <rencyngina@gmail.com>
2024-07-30 23:12:42 +0300

    document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Add any custom head tags here */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://example.com/styles.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '@/Components/Navbar';
 import Hero from '@/Components/HeroSection/Hero';
 
+
 const Index = () => {
   return (
     <main>
